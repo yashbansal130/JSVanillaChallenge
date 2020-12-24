d68cd69cd034041c71aeebd6c0ae4116f3c0bc41
Yash Bansal <bansal.yash.130@gmail.com>
2020-12-24 21:36:13 +0530

    analog clock completed

---
+++ analog clock/clock.js
@@ -0,0 +1,21 @@
+const secHand=document.querySelector('.second-hand');
+const minHand=document.querySelector('.min-hand');
+const hourHand=document.querySelector('.hour-hand');
+function setDate(){
+    const now=new Date();
+    const sec=now.getSeconds();
+    const secDegrees=((sec/60)*360)+90;
+    secHand.style.transform=`rotate(${secDegrees}deg)`;
+    
+    
+    const min=now.getMinutes();
+    const minDegrees=((min/60)*360)+90;
+    minHand.style.transform=`rotate(${minDegrees}deg)`;
+    
+                       
+    const hour=now.getHours();
+    const hourDegrees=((hour/12)*360)+90;
+    hourHand.style.transform=`rotate(${hourDegrees}deg)`;
+    
+}
+setInterval(setDate,1000);
